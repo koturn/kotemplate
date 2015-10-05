648571ca3a1d913e86675e0be11a0c79bcf20554
koturn <jeak.koutan.apple@gmail.com>
2015-10-06 01:37:27 +0900

    Add templates of Windows application

---
+++ Cpp/winapp.cpp
@@ -0,0 +1,121 @@
+#include <windows.h>
+
+#if defined(_MSC_VER) && !defined(DISABLE_PRAGMA_LINK)
+#  pragma comment(lib, "gdi32")
+#  pragma comment(lib, "user32")
+#endif
+
+#ifndef __UNUSED__
+#  if defined(__cplusplus)
+#    define __UNUSED__(x)
+#  elif defined(__GNUC__)
+#    define __UNUSED__(x)  __UNUSED___ ## x __attribute__((unused))
+#  elif defined(_MSC_VER)
+#    define __UNUSED__(x)  __pragma(warning(suppress: 4100)) x
+#  elif defined(__LCLINT__)
+#    define __UNUSED__(x)  /*@unused@*/ x
+#  else
+#    define __UNUSED__(x)  x
+#  endif
+#endif
+
+LRESULT CALLBACK WndProc(HWND hCurInst, UINT hPrevInst, WPARAM lpsCmdLine, LPARAM nCmdShow);
+ATOM InitApp(HINSTANCE hInst);
+BOOL InitInstance(HINSTANCE hInst, int nCmdShow);
+
+TCHAR szClassName[] = TEXT("<+FILEBASE+>");
+
+
+int WINAPI
+WinMain(HINSTANCE hCurInst, HINSTANCE __UNUSED__(hPrevInst), LPSTR __UNUSED__(lpsCmdLine), int nCmdShow)
+{
+  if (!InitApp(hCurInst)) return FALSE;
+  if (!InitInstance(hCurInst, nCmdShow)) return FALSE;
+
+  MSG msg;
+  BOOL bRet;
+  while ((bRet = GetMessage(&msg, NULL, 0, 0)) != 0) {
+    if (bRet == -1) {
+      MessageBox(NULL, TEXT("GetMessage Error"), TEXT("error"), MB_OK);
+      break;
+    } else {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+  }
+  return static_cast<int>(msg.wParam);
+}
+
+
+ATOM
+InitApp(HINSTANCE hInst)
+{
+  WNDCLASSEX wc;
+  wc.cbSize = sizeof(WNDCLASSEX);
+  wc.style = CS_HREDRAW | CS_VREDRAW;
+  wc.lpfnWndProc = WndProc;
+  wc.cbClsExtra = 0;
+  wc.cbWndExtra = 0;
+  wc.hInstance = hInst;
+  wc.hIcon = static_cast<HICON>(LoadImage(
+      NULL,
+      MAKEINTRESOURCE(IDI_APPLICATION),
+      IMAGE_ICON,
+      0,
+      0,
+      LR_DEFAULTSIZE | LR_SHARED));
+  wc.hCursor = static_cast<HCURSOR>(LoadImage(
+      NULL,
+      MAKEINTRESOURCE(IDC_ARROW),
+      IMAGE_CURSOR,
+      0,
+      0,
+      LR_DEFAULTSIZE | LR_SHARED));
+  wc.hbrBackground = static_cast<HBRUSH>(GetStockObject(WHITE_BRUSH));
+  wc.lpszMenuName = NULL;
+  wc.lpszClassName = szClassName;
+  wc.hIconSm = static_cast<HICON>(LoadImage(
+      NULL,
+      MAKEINTRESOURCE(IDI_APPLICATION),
+      IMAGE_ICON,
+      0,
+      0,
+      LR_DEFAULTSIZE | LR_SHARED));
+  return RegisterClassEx(&wc);
+}
+
+
+BOOL
+InitInstance(HINSTANCE hInst, int nCmdShow)
+{
+  HWND hWnd = CreateWindow(
+      szClassName,
+      TEXT("<+FILEBASE+>"),
+      WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT,  /* x-coordinate */
+      CW_USEDEFAULT,  /* y-coordinate */
+      CW_USEDEFAULT,  /* width */
+      CW_USEDEFAULT,  /* height */
+      NULL,
+      NULL,
+      hInst,
+      NULL);
+  if (!hWnd) return FALSE;
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
+  return TRUE;
+}
+
+
+LRESULT CALLBACK
+WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+  switch (msg) {
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+    default:
+      return DefWindowProc(hWnd, msg, wp, lp);
+  }
+  return 0;
+}
