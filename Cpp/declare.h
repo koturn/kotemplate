425aa9921544bd60bd26f2429a41deb2e156bd34
koturn <jeak.koutan.apple@gmail.com>
2015-12-28 19:11:19 +0900

    Enable to treat with std::cout and std::cin

--- Cpp/declare.h
+++ Cpp/declare.h
@@ -10,18 +10,46 @@
 #ifndef <+FILE_CAPITAL+>_H
 #define <+FILE_CAPITAL+>_H
 
+#include <iostream>
+
 
 /*!
  * @brief Template class
  */
-class <+FILEBASE+>
+class <+FILE_PASCAL+>
 {
 private:
 public:
   <+FILEBASE+>() {
     <+CURSOR+>
   }
-};  // class <+FILEBASE+>
+
+  template<typename CharT, typename Traits>
+    friend std::basic_ostream<CharT, Traits>&
+    operator<<(std::basic_ostream<CharT, Traits>& os, const CoutTest& this_);
+
+  template<typename CharT, typename Traits>
+    friend std::basic_istream<CharT, Traits>&
+    operator>>(std::basic_istream<CharT, Traits>& is, CoutTest& this_);
+};  // class <+FILE_PASCAL+>
+
+
+template<typename CharT, typename Traits>
+std::basic_ostream<CharT, Traits>&
+operator<<(std::basic_ostream<CharT, Traits>& os, const CoutTest& this_)
+{
+
+  return os;
+}
+
+
+template<typename CharT, typename Traits>
+std::basic_istream<CharT, Traits>&
+operator>>(std::basic_istream<CharT, Traits>& is, CoutTest& this_)
+{
+
+  return is;
+}
 
 
 #endif  // <+FILE_CAPITAL+>_H
