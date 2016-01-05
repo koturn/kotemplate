88abbdc97f9a72ccfff987c18193c791d9bdae01
koturn <jeak.koutan.apple@gmail.com>
2016-01-06 04:15:44 +0900

    Implement copy-constructor, destructor and assignment-overload

--- Cpp/declare.h
+++ Cpp/declare.h
@@ -20,23 +20,51 @@ class <+FILE_PASCAL+>
 {
 private:
 public:
-  <+FILEBASE+>() {
-    <+CURSOR+>
-  }
+  <+FILE_PASCAL+>();
+  <+FILE_PASCAL+>(const <+FILE_PASCAL+>& that);
+  ~<+FILE_PASCAL+>();
+
+  <+FILE_PASCAL+>& operator=(const <+FILE_PASCAL+>& that);
 
   template<typename CharT, typename Traits>
     friend std::basic_ostream<CharT, Traits>&
-    operator<<(std::basic_ostream<CharT, Traits>& os, const CoutTest& this_);
+    operator<<(std::basic_ostream<CharT, Traits>& os, const <+FILE_PASCAL+>& this_);
 
   template<typename CharT, typename Traits>
     friend std::basic_istream<CharT, Traits>&
-    operator>>(std::basic_istream<CharT, Traits>& is, CoutTest& this_);
+    operator>>(std::basic_istream<CharT, Traits>& is, <+FILE_PASCAL+>& this_);
 };  // class <+FILE_PASCAL+>
+
+
+<+FILE_PASCAL+>::<+FILE_PASCAL+>()
+{
+  <+CURSOR+>
+}
+
+
+<+FILE_PASCAL+>::<+FILE_PASCAL+>(const <+FILE_PASCAL+>& that)
+{
+
+}
+
+
+<+FILE_PASCAL+>::~<+FILE_PASCAL+>()
+{
+
+}
+
+
+<+FILE_PASCAL+>&
+<+FILE_PASCAL+>::operator=(const <+FILE_PASCAL+>& that)
+{
+
+  return *this;
+}
 
 
 template<typename CharT, typename Traits>
 std::basic_ostream<CharT, Traits>&
-operator<<(std::basic_ostream<CharT, Traits>& os, const CoutTest& this_)
+operator<<(std::basic_ostream<CharT, Traits>& os, const <+FILE_PASCAL+>& this_)
 {
 
   return os;
@@ -45,7 +73,7 @@ operator<<(std::basic_ostream<CharT, Traits>& os, const CoutTest& this_)
 
 template<typename CharT, typename Traits>
 std::basic_istream<CharT, Traits>&
-operator>>(std::basic_istream<CharT, Traits>& is, CoutTest& this_)
+operator>>(std::basic_istream<CharT, Traits>& is, <+FILE_PASCAL+>& this_)
 {
 
   return is;
