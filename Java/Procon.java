e7dec9340612552eb9bb921f339f77f6b84debe5
koturn <jeak.koutan.apple@gmail.com>
2016-01-08 23:25:44 +0900

    Add programming contest templates for Java

---
+++ Java/Procon.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class <+FILE+> {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			String[] tokens = line.split(" ");
+			<+CURSOR+>
+		}
+	}
+}
---
+++ Java/ProconScanner.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class <+FILE+> {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()) {
+			int n = Integer.parseInt(sc.next());
+			<+CURSOR+>
+		}
+	}
+}
