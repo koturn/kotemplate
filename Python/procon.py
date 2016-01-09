16941d3092bcc779d7d305a4641be5bbbacc7b92
koturn <jeak.koutan.apple@gmail.com>
2016-01-10 04:13:46 +0900

    Add a programming contest template for Python2 amd Python3

---
+++ Python/procon.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import fileinput
+
+
+if __name__ == '__main__':
+    for line in fileinput.input():
+        tokens = line.strip().split()
+        <+CURSOR+>
