fa085e99e6941698c3b7aa308dc7872a9fb0bdc0
koturn <jeak.koutan.apple@gmail.com>
2016-01-22 09:24:06 +0900

    Add programming contest templates for Scheme

---
+++ Scheme/procon.scm
@@ -0,0 +1,12 @@
+(define (main-function)
+  (letrec ((main-loop (lambda ()
+                        (let ((a (read)) (b (read)))
+                          (if (or (eof-object? a) (eof-object? b))
+                            #f
+                            (begin (display a)
+                                   (display a)
+                                   <+CURSOR+>
+                                   (main-loop)))))))
+    (main-loop)))
+
+(main-function)
