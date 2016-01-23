562600b195af3b0801b6e701a4c3eabb0bb77aa2
koturn <jeak.koutan.apple@gmail.com>
2016-01-24 04:13:51 +0900

    Add a programming contest template for Vim Script on bash

---
+++ sh/bash/procon_vimscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+
+vim -u NONE -i NONE -N -n -e -s -S <(cat <<EOF
+function! s:main(input) abort
+  <+CURSOR+>
+  return 'answer'
+endfunction
+
+let s:input = getline(1, '$')
+enew
+put =s:main(s:input)
+1 delete _
+%print
+EOF
+) <(cat)
