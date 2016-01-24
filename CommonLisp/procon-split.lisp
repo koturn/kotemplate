d8849a84d32f1449aba26ad0c5c365d132f8260d
koturn <jeak.koutan.apple@gmail.com>
2016-01-25 00:07:16 +0900

    Improve programming contest templates for Common Lisp

--- CommonLisp/procon-split.lisp
+++ CommonLisp/procon-split.lisp
@@ -1,12 +1,19 @@
 (defun split-by-one-space (string)
+  "Split string by one space"
+  (declare (optimize (safety 0) (space 0) (debug 0) (speed 3)))
   (loop for i = 0 then (1+ j)
         as j = (position #\Space string :start i)
         collect (subseq string i j)
         while j))
 
 (defun main ()
-  (print (mapcar #'parse-integer
-                 (split-by-one-space (read-line))))
-  <+CURSOR+>)
+  "Main function"
+  (declare (optimize (safety 0) (space 0) (debug 0) (speed 3)))
+  (loop for line = nil then
+        (let ((tokens (mapcar #'parse-integer
+                              (split-by-one-space line))))
+          (princ tokens)
+          <+CURSOR+>)
+        until (null (setq line (read-line)))))
 
 (main)
--- CommonLisp/procon.lisp
+++ CommonLisp/procon.lisp
@@ -1,7 +1,11 @@
 (defun main ()
   "Main function"
-  (print (read))
-  (print (read-line))
-  <+CURSOR+>)
+  (declare (optimize (safety 0) (space 0) (debug 0) (speed 3)))
+  (loop for a = nil then b
+        and b = nil then
+        (progn (format t "~A ~A ~%" a b)
+               <+CURSOR+>)
+        until (or (null (setq a (read)))
+                  (null (setq b (read))))))
 
 (main)
