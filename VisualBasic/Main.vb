8193684cfcb5a8ab69d217976ddc82ecca683fe4
koturn <jeak.koutan.apple@gmail.com>
2016-01-26 00:16:14 +0900

    Add main templates for VB.NET

---
+++ VisualBasic/Main.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Public Class MainClass
+    Shared Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        <+CURSOR+>
+    End Sub
+End Class
---
+++ VisualBasic/MainModule.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Module MainModule
+    Sub Main(args As String())
+        Console.WriteLine("Hello World!")
+        <+CURSOR+>
+    End Sub
+End Module
