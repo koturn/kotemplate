739441a04defc25ae0a2a79607832a810e656ee0
koturn <jeak.koutan.apple@gmail.com>
2016-02-01 13:01:03 +0900

    Use (newline)

--- Scheme/procon.scm
+++ Scheme/procon.scm
@@ -1,11 +1,13 @@
 (define (main-function)
   (letrec ((main-loop (lambda ()
                         (let ((a (read)) (b (read)))
-                          (if (or (eof-object? a) (eof-object? b))
+                          (if (or (eof-object? a)
+                                  (eof-object? b))
                             #f
                             (begin (display a)
+                                   (display " ")
                                    (display b)
-                                   <+CURSOR+>
+                                   (newline)
                                    (main-loop)))))))
     (main-loop)))
 
